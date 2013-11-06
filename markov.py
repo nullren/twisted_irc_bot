ea8854306eaf096651b0376e561333d52cc7e318
nullren <chopflop@gmail.com>
2013-11-06 15:06:10 -0800

    i dunno

--- markov.py
+++ markov.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python2
 
 import boat
+import collections
+
+START='__________..START'
+STOP='__________..STOP'
+brain = collections.defaultdict(set)
+
+def add_to_brain(msg):
+  words = [START] + msg.split() + [STOP]
+  
 
 def onmsg(boat, user, channel, msg):
+  add_to_brain(msg)
   boat.msg(channel, "lol, you said: {}".format(msg))
 
 def main():
